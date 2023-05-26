d723d78b66a0f405bf9a1f02c92739ce461ae334
sugihara ryoto <sugihara ryoto>
2023-05-26 16:25:50 +0900

    2-1-4を行いました。

---
+++ Chapter2-1/Chapter2-1 問4.sql
@@ -0,0 +1,19 @@
+SELECT
+	u.l_name,
+	u.f_name,
+	COUNT(*)
+FROM
+	rental AS r
+INNER JOIN
+	usr AS u
+ON
+	r.user_id=u.user_id
+WHERE
+	r.returned = 0
+GROUP BY
+	u.l_name,
+	u.f_name,
+	r.user_id
+ORDER BY
+	COUNT(r.user_id) DESC
+;
